015ee5fbe0ebecffdb77611df72d97b80cc0938b
vjcspy <mr.vjcspy@gmail.com>
2016-06-03 11:36:49 +0700

    - init Test Router

--- Http/Controllers/IzAdminController.php
+++ Http/Controllers/IzAdminController.php
@@ -1,12 +1,38 @@
 <?php namespace Modules\Izadmin\Http\Controllers;
 
-use Pingpong\Modules\Routing\Controller;
+use Modules\IzCore\Http\Controllers\ThemeAbstractController;
 
-class IzAdminController extends Controller {
-	
-	public function index()
-	{
-		return view('izadmin::index');
-	}
-	
+class IzAdminController extends ThemeAbstractController {
+
+    public function getTest() {
+        $this->setTheme('admin.default')->setLayout('default');
+
+        $this->addAssets(
+            [
+                'angular-material',
+                'angular',
+                'bootstrap',
+                'jquery',
+                'angular-aria',
+                'angular-messages',
+                'angular-animate',
+                'angular-ui-router'
+            ]);
+
+        $this->setViewData(
+            [
+                'view_data' => 'View Data Test'
+            ]
+        );
+
+        $this->setThemeData(
+            [
+                'title' => 'Admin Title Test'
+            ]
+        );
+
+        // will render view: 'layout/router/current_function.blade.php';
+        return $this->renderHtml();
+    }
+
 }
---
+++ themes/admin.default/views/README.md
@@ -0,0 +1,3 @@
+# Cấu trúc thư mục 
+ - view/layout_name/router_name1/router_name2/action_name.blade.php
+ 
