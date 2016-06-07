654a868f92ee5a910b0f89c03d58b7117cf108ae
vjcspy <mr.vjcspy@gmail.com>
2016-06-07 11:58:24 +0700

    - nothing

---
+++ Repositories/CoreConfig/AdminConfig.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vjcspy
+ * Date: 06/06/2016
+ * Time: 10:50
+ */
+
+namespace Modules\IzAdmin\Repositories\CoreConfig;
+
+
+use Modules\IzCore\Repositories\CoreConfig\ConfigInterface;
+
+class AdminConfig implements ConfigInterface {
+
+    public function handle() {
+        // TODO: Implement handle() method.
+        return [
+            'path'        => 'modules/themes',
+            'appFuncName' => 'Admin Function',
+            'base_url'    => url('/')
+        ];
+    }
+}
